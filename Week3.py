4f1d7c80448c32f7defa8d4e7bfeb531a189b36d
Karolina Ruiz Rogelj <76909925+karolina-vruizrogelj@users.noreply.github.com>
2021-05-20 15:20:15 +0200

    Add files via upload

---
+++ Week3.py
@@ -0,0 +1,17 @@
+# Karolina Ruiz Rogelj 
+# Python Review
+# Week 3 of PythonforEverybody
+# scripts are stored text in a python file 
+# interactive 
+
+# Conditional ; if ; will result in true or false 
+
+# While loop; if true then rude code, if not then print the other thing 
+# n is the iteration variable; it changes each time through a loop
+n = 5 
+while n > 5:
+    print(n)
+    n = n - 1
+print("Blastoff")
+
+# Will print 5, 4, 3, 2, 1 and then Blastoff 
